733369765da5b15a39b29f2de33b604deec1090b
zainabdnaya <diyanazizo13@gmail.com>
2021-11-02 17:41:06 +0100

    retry1003

--- src/Mariadb/setup.sh
+++ src/Mariadb/setup.sh
@@ -1,6 +1,7 @@
 service mysql start
-mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+
+mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -u root -e "CREATE DATABASE wordp;"
-mysql -u root -e "USE $WP_DB_NAME; GRANT ALL PRIVILEGES ON * TO '$MYSQL_USER'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-mysql -u root wordp < /wordpress.sql
-cp -r /var/lib/mysql /
+# mysql -u root wordp < /wordpress.sql
+mysql -u root -e "USE wordp; GRANT ALL PRIVILEGES ON * TO '${MYSQL_USER}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+
--- src/wordpress/start.sh
+++ src/wordpress/start.sh
@@ -1,1 +1,6 @@
+ wget http://wordpress.org/latest.tar.gz &&\
+    tar -xzvf latest.tar.gz &&\
+    rm latest.tar.gz && cp -rf /wordpress/* /var/www/html && cp -rf  /wp-config.php /var/www/html
+
+
 php-fpm7.3 --nodaemonize
