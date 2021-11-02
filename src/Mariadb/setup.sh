50854f9b6dc9cc36a2d8491a5f4d9241bc84800c
zdnaya <zdnaya@student.1337.ma>
2021-11-02 15:38:04 +0100

    rety1002

--- src/Mariadb/setup.sh
+++ src/Mariadb/setup.sh
@@ -2,5 +2,5 @@ service mysql start
 mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 mysql -u root -e "CREATE DATABASE wordp;"
 mysql -u root -e "USE $WP_DB_NAME; GRANT ALL PRIVILEGES ON * TO '$MYSQL_USER'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-
+mysql -u root wordp < /wordpress.sql
 cp -r /var/lib/mysql /
